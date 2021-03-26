4e1fc887f7d7509932da1d3744b5010517d85cce
gregaswright <greg.a.s.wright@gmail.com>
2021-03-25 18:08:22 -0700

    final code commit, sort by favorites was close I felt

--- src/Container/CourseContainer.js
+++ src/Container/CourseContainer.js
@@ -5,12 +5,14 @@ import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 import CardGroup from 'react-bootstrap/CardGroup'
 import Modal from 'react-bootstrap/Modal'
+import Nav from 'react-bootstrap/Nav'
 
 class CourseContainer extends React.Component {
 
     state = {
         favoriteArray: [],
-        savedClicked: false
+        savedClicked: false,
+        sortByFavoritesClicked: false
     }
 
 
@@ -26,6 +28,17 @@ class CourseContainer extends React.Component {
         return this.state.favoriteArray.map(favoriteObj => <Favorite key={favoriteObj.id} favoriteObj={favoriteObj} removeFromFavorite={this.removeFromFavorite}/>)
     }
 
+
+    // this is not working but I felt close
+    sortByFavorites = () => {
+        let filteredArray = this.state.favoriteArray.filter(course => this.props.coursesArray.includes(course))
+        return filteredArray.map(courseObj => <Course key={courseObj.id} courseObj={courseObj} collectFavorites={this.collectFavorites} removeFromFavorite={this.removeFromFavorite}/>)
+    }
+
+    sortByFavoritesCLickHandler = () => {
+        this.setState({ sortByFavoritesClicked: !this.state.sortByFavoritesClicked})
+    }
+
     savedClickedHandler = () => {
         this.setState({ savedClicked: !this.state.savedClicked})
       }
@@ -38,11 +51,15 @@ class CourseContainer extends React.Component {
     }
 
     render() {
+        console.log(this.sortByFavorites())
         return(
             <div>
                 <div>
                     <Navbar bg="dark" variant="dark">
                     <Navbar.Brand href="#home">Masterclass</Navbar.Brand>
+                    <Nav.Item>
+                        <Nav.Link onClick={this.sortByFavoritesCLickHandler}>Sort by Favorites</Nav.Link>
+                    </Nav.Item>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
                         <Button variant="secondary" onClick={this.savedClickedHandler}>Saved Classes: {this.state.favoriteArray.length}</Button>
@@ -50,7 +67,7 @@ class CourseContainer extends React.Component {
                     </Navbar>
                 </div>
                 <CardGroup>
-                    {this.renderCourses()}
+                    {this.sortByFavoritesClicked ? this.sortByFavorites() : this.renderCourses()}
                 </CardGroup>
                 <Modal show={this.state.savedClicked} onHide={this.savedClickedHandler}>
                     <Modal.Header closeButton>
